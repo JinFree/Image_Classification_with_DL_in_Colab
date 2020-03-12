cfc617ed929a3e628a83a1be03badc46a7869147
Jin Yeong Wang <ytrqwe12@gmail.com>
2020-03-12 23:39:30 +0900

    Files for Inference Test

---
+++ PyTorch/pytorch_network_model.py
@@ -0,0 +1,17 @@
+import torch
+from torchvision import models
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MODEL(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.network = models.mobilenet_v2(pretrained=False)
+        self.classifier = nn.Sequential(
+            nn.Dropout()
+            , nn.Linear(1000, num_classes)
+            , nn.Sigmoid()
+        )
+    def forward(self, x):
+        x = self.network(x)
+        return self.classifier(x)
